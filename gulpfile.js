3de19ea98136ceca98ae826285fb0ca530d4f224
“[Emmanuel <emmanuelzamudio14@gmail.com>
2021-06-15 17:37:35 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+function css() {
+    return gulp
+        .src('assets/css/app.scss')
+        .pipe(autoprefixer({
+            overrideBrowserslist : ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sass({
+            outputStyle: 'compact', // expanded, nested, compact, compressed
+        }))
+        .pipe( gulp.dest('assets/css') );
+}
+
+function watchFiles() {
+    gulp.watch('assets/css/*.scss', css); 
+    gulp.watch('index.html');
+}
+
+
+// Registrar funciones como tareas
+
+gulp.task( 'css', css );
+gulp.task('watch', gulp.parallel(watchFiles) );
